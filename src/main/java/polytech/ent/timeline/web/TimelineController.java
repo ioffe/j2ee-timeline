dd442164dc9e29f5c2da7c73c08d7356f5ef2215
Dima Kozhevnikov <dima.kozhevnikov@gmail.com>
2011-05-18 11:32:17 +0400

    Timeline delete

--- src/main/java/polytech/ent/timeline/web/TimelineController.java
+++ src/main/java/polytech/ent/timeline/web/TimelineController.java
@@ -4,6 +4,7 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import polytech.ent.timeline.service.TimelineService;
@@ -17,5 +18,11 @@ public class TimelineController {
 	public String timeline(Map<String, Object> map) {
 		map.put("tls", tlService.listTimeline());
 		return "timeline";
+	}
+	
+	@RequestMapping("/timeline/delete/{tlid}") 
+	public String deleteTL(@PathVariable("tlid") Integer tlid) {
+		tlService.removeTimeline(tlid);
+		return "redirect:/timeline";
 	}
 }
